b8bf16b2a3433a4f29c73544c900cac45b8441ab
Shan-e-Zehra <shanezehra117@gmail.com>
2024-12-17 13:34:54 +0500

    bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Footer from "@/components/footer";
+import Footer from "@/components/footer"
 import Header from "@/components/header";
 
 const geistSans = localFont({
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { FaFacebook } from "react-icons/fa";
 import Link from "next/link";
-import { motion } from "framer-motion"; // Import framer-motion for animation
+import { motion } from "framer-motion"; // Import motion for animation
 
 export default function Footer() {
   return (
@@ -25,7 +25,10 @@ export default function Footer() {
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 0.3 }}
         >
-          A perfume is a liquid mixture used to emit a pleasant odour. It is formed from fragrant essential oils derived from plants and spices or synthetic aromatic compounds. Cosmetic fragrances applied to a person's body to emit a pleasant smell include perfume.
+          A perfume is a liquid mixture used to emit a pleasant odour. It is
+          formed from fragrant essential oils derived from plants and spices or
+          synthetic aromatic compounds. Cosmetic fragrances applied to a persons
+          body to emit a pleasant smell include perfume.
         </motion.li>
         <motion.li
           className="facebook flex items-center space-x-2"
@@ -42,7 +45,10 @@ export default function Footer() {
           >
             Visit our Facebook page
           </Link>
-          <FaFacebook size={20} className="text-blue-600 hover:text-blue-500 transition duration-300" />
+          <FaFacebook
+            size={20}
+            className="text-blue-600 hover:text-blue-500 transition duration-300"
+          />
         </motion.li>
       </motion.ul>
     </motion.div>
